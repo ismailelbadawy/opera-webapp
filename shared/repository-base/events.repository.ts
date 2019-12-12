4b701890de865ae9f8beca6fea37042ddd38848c
ismailhelmy <38408394+ismailhelmy@users.noreply.github.com>
2019-12-12 23:23:30 +0200

    added some functions in the events repository

--- shared/repository-base/events.repository.ts
+++ shared/repository-base/events.repository.ts
@@ -1,3 +1,9 @@
-export abstract class IEventsReposioty{
+export abstract class IEventsRepository{
     
+    abstract createEvent(event:Event):Promise<Event>;
+    abstract cancelEvent(eventId:string):Promise<boolean>;
+    //Upload poster somehow 
+    abstract editEvent(event:Event):Promise<Event>;
+    abstract viewAvailableEvents():Promise<Event[]>;
+
 }
