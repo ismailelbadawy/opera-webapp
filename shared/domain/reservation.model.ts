087386b27bf58b927d8c9b3728d596b486a85f1d
ismailhelmy <38408394+ismailhelmy@users.noreply.github.com>
2019-12-12 23:37:43 +0200

    Added setters and getters to models

--- shared/domain/reservation.model.ts
+++ shared/domain/reservation.model.ts
@@ -1,11 +1,57 @@
 export class Reservation {
+    public get pinNumber(): string | null {
+        return this._pinNumber;
+    }
+    public set pinNumber(value: string | null) {
+        this._pinNumber = value;
+    }
+    public get cardNumber(): string | null {
+        return this._cardNumber;
+    }
+    public set cardNumber(value: string | null) {
+        this._cardNumber = value;
+    }
+    public get seat(): Seat | null {
+        return this._seat;
+    }
+    public set seat(value: Seat | null) {
+        this._seat = value;
+    }
+    public get userId(): string | null {
+        return this._userId;
+    }
+    public set userId(value: string | null) {
+        this._userId = value;
+    }
+    public get eventId(): string | null {
+        return this._eventId;
+    }
+    public set eventId(value: string | null) {
+        this._eventId = value;
+    }
+    public get ticketId(): string | null {
+        return this._ticketId;
+    }
+    public set ticketId(value: string | null) {
+        this._ticketId = value;
+    }
     constructor(
-        private _ticketId: string,
-        private _eventId: string,
-        private _userId:string,
-        private _row: number,
-        private _column: number
+        private _ticketId: string | null,
+        private _eventId: string | null,
+        private _userId: string | null,
+        private _seat: Seat | null,
+        private _cardNumber: string | null,
+        private _pinNumber: string | null
     ) {
 
     }
+}
+
+export class Seat{
+    constructor(
+        private _row:number|null,
+        private _column:number|null
+        ){
+
+        }
 }
--- shared/domain/user.model.ts
+++ shared/domain/user.model.ts
@@ -1,17 +1,89 @@
 export class User {
+    public get paswordSalt(): string | null {
+        return this._paswordSalt;
+    }
+    public set paswordSalt(value: string | null) {
+        this._paswordSalt = value;
+    }
+    public get passwordHash(): string | null {
+        return this._passwordHash;
+    }
+    public set passwordHash(value: string | null) {
+        this._passwordHash = value;
+    }
+    public get address(): string | null {
+        return this._address;
+    }
+    public set address(value: string | null) {
+        this._address = value;
+    }
+    public get email(): string {
+        return this._email;
+    }
+    public set email(value: string) {
+        this._email = value;
+    }
+    public get city(): string | null {
+        return this._city;
+    }
+    public set city(value: string | null) {
+        this._city = value;
+    }
+    public get gender(): string | null {
+        return this._gender;
+    }
+    public set gender(value: string | null) {
+        this._gender = value;
+    }
+    public get birthDate(): string | null {
+        return this._birthDate;
+    }
+    public set birthDate(value: string | null) {
+        this._birthDate = value;
+    }
+    public get lastName(): string | null {
+        return this._lastName;
+    }
+    public set lastName(value: string | null) {
+        this._lastName = value;
+    }
+    public get firstName(): string | null {
+        return this._firstName;
+    }
+    public set firstName(value: string | null) {
+        this._firstName = value;
+    }
+    public get username(): string {
+        return this._username;
+    }
+    public set username(value: string) {
+        this._username = value;
+    }
+    public get userType(): UserType {
+        return this._userType;
+    }
+    public set userType(value: UserType) {
+        this._userType = value;
+    }
+    public get userId(): string {
+        return this._userId;
+    }
+    public set userId(value: string) {
+        this._userId = value;
+    }
    constructor(
-       private _userId:string,
+       private _userId: string,
        private _userType: UserType,
-       private _username:string,
-       private _firstName:string|null,
-       private _lastName:string|null,
-       private _birthDate: string|null,
-       private _gender: string|null,
-       private _city: string|null,
-       private _email:string,
-       private _address: string|null,
-       private _passwordHash : string|null,
-       private _paswordSalt:string|null
+       private _username: string,
+       private _firstName: string | null,
+       private _lastName: string | null,
+       private _birthDate: string | null,
+       private _gender: string | null,
+       private _city: string | null,
+       private _email: string,
+       private _address: string | null,
+       private _passwordHash: string | null,
+       private _paswordSalt: string | null
 
        ){
 
