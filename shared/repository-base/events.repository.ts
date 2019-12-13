ae51e538c289d74e4e320886789ce35c75babd83
Ismail El Badawy <ismailelbadawyw@gmail.com>
2019-12-13 14:22:58 +0200

    added a function to attach the poster url to the event.

--- shared/repository-base/events.repository.ts
+++ shared/repository-base/events.repository.ts
@@ -2,7 +2,7 @@ export abstract class IEventsRepository{
     
     abstract createEvent(event:Event):Promise<Event>;
     abstract cancelEvent(eventId:string):Promise<boolean>;
-    //Upload poster somehow 
+    abstract uploadPosterUrl(eventId: string, posterUrl : string) : Promise<boolean>;
     abstract editEvent(event:Event):Promise<Event>;
     abstract viewAvailableEvents():Promise<Event[]>;
 
