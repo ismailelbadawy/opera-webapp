a92eff99a05162da047c924ae39ccf84d51e9e91
Ismail El Badawy <ismailelbadawyw@gmail.com>
2019-12-13 14:29:37 +0200

    Added a concrete implementation of the events repo as an example.

---
+++ backend/src/data/database-events.repository.ts
@@ -0,0 +1,21 @@
+import { IEventsRepository } from 'shared/repository-base/events.repository';
+
+export class DatabaseEventsRepository implements IEventsRepository {
+    createEvent(event: Event): Promise<Event> {
+        throw new Error("Method not implemented.");
+    }    
+    cancelEvent(eventId: string): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+    uploadPosterUrl(eventId: string, posterUrl: string): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+    editEvent(event: Event): Promise<Event> {
+        throw new Error("Method not implemented.");
+    }
+    viewAvailableEvents(): Promise<Event[]> {
+        throw new Error("Method not implemented.");
+    }
+
+    
+}
