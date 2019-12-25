297a59b94430a6166d3eb78fe288f9d94839fca2
muhammad20 <muhammadahmad201196@hotmail.com>
2019-12-25 02:51:22 +0200

    added event details screen route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { EventDetailsComponent } from './event-details/event-details.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component : LoginComponent
+  },
+  {
+    path:'event-details',
+    component: EventDetailsComponent
   }
 ];
 
